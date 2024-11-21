961d507b35381b85c0288d3d0f0dc2be0fc31864
nader <naedrkamal4@gmail.com>
2024-11-21 08:14:53 +0300

    .

---
+++ lang/en/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+
+    'اجمالي المبيعات' => 'Total sales',
+    'اجمالي الطلبات' => 'Total requests',
+    'عدد المنتجات'=> 'Number of Products',
+    'عدد العملاء'=> 'Number of Customers',
+    'احدث الطلبات'=>'Latest Orders',
+    'اخر التقيمات'=> 'Latest Reviews'
+]
+;
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -7,7 +7,7 @@
             </div>
             <div class="content-body">
                 <div id="crypto-stats-3" class="row">
-                    <div class="col-xl-4 col-12">
+                    <div class="col-xl-3 col-12">
                         <div class="card crypto-card-3 pull-up">
                             <div class="card-content">
                                 <div class="card-body pb-0">
@@ -16,12 +16,10 @@
                                             <h1><i class="cc BTC warning font-large-2" title="BTC"></i></h1>
                                         </div>
                                         <div class="col-5 pl-2">
-                                            <h4>BTC</h4>
-                                            <h6 class="text-muted">Bitcoin</h6>
+                                            <h4>{{ __('dashboard.اجمالي المبيعات') }}</h4>
                                         </div>
                                         <div class="col-5 text-right">
                                             <h4>$9,980</h4>
-                                            <h6 class="success darken-4">31% <i class="la la-arrow-up"></i></h6>
                                         </div>
                                     </div>
                                 </div>
@@ -33,7 +31,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 col-12">
+                    <div class="col-xl-3 col-12">
                         <div class="card crypto-card-3 pull-up">
                             <div class="card-content">
                                 <div class="card-body pb-0">
@@ -42,12 +40,10 @@
                                             <h1><i class="cc ETH blue-grey lighten-1 font-large-2" title="ETH"></i></h1>
                                         </div>
                                         <div class="col-5 pl-2">
-                                            <h4>ETH</h4>
-                                            <h6 class="text-muted">Ethereum</h6>
+                                            <h4>{{ __('dashboard.اجمالي المبيعات') }}</h4>
                                         </div>
                                         <div class="col-5 text-right">
                                             <h4>$944</h4>
-                                            <h6 class="success darken-4">12% <i class="la la-arrow-up"></i></h6>
                                         </div>
                                     </div>
                                 </div>
@@ -59,7 +55,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 col-12">
+                    <div class="col-xl-3 col-12">
                         <div class="card crypto-card-3 pull-up">
                             <div class="card-content">
                                 <div class="card-body pb-0">
@@ -68,12 +64,36 @@
                                             <h1><i class="cc XRP info font-large-2" title="XRP"></i></h1>
                                         </div>
                                         <div class="col-5 pl-2">
-                                            <h4>XRP</h4>
-                                            <h6 class="text-muted">Balance</h6>
+                                            <h4>{{ __('dashboard.عدد العملاء') }}</h4>
                                         </div>
                                         <div class="col-5 text-right">
                                             <h4>$1.2</h4>
-                                            <h6 class="danger">20% <i class="la la-arrow-down"></i></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="xrp-chartjs" class="height-75"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-12">
+                        <div class="card crypto-card-3 pull-up">
+                            <div class="card-content">
+                                <div class="card-body pb-0">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <h1><i class="cc XRP info font-large-2" title="XRP"></i></h1>
+                                        </div>
+                                        <div class="col-5 pl-2">
+                                            <h4>{{ __('dashboard.عدد المنتجات') }}</h4>
+                                        </div>
+                                        <div class="col-5 text-right">
+                                            <h4>$1.2</h4>
+
                                         </div>
                                     </div>
                                 </div>
@@ -90,14 +110,11 @@
 
                 <!-- Sell Orders & Buy Order -->
                 <div class="row match-height">
-                    <div class="col-12 col-xl-6">
+                    <div class="col-12 col-xl-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Sell Order</h4>
+                                <h4 class="card-title">{{ __('dashboard.احدث الطلبات') }}</h4>
                                 <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-                                <div class="heading-elements">
-                                    <p class="text-muted">Total BTC available: 6542.56585</p>
-                                </div>
                             </div>
                             <div class="card-content">
                                 <div class="table-responsive">
@@ -146,14 +163,11 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-xl-6">
+                    <div class="col-12 col-xl-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Buy Order</h4>
+                                <h4 class="card-title">{{ __('dashboard.اخر التقيمات') }}</h4>
                                 <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-                                <div class="heading-elements">
-                                    <p class="text-muted">Total USD available: 9065930.43</p>
-                                </div>
                             </div>
                             <div class="card-content">
                                 <div class="table-responsive">
